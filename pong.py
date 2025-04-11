36b9e85c0b4a6e094e82e0fe19962eafb870d946
Samuel Lanzuela <amlanzuela@gmail.com>
2025-04-11 13:50:10 -0700

    We're so ponging

--- pong.py
+++ pong.py
@@ -3,7 +3,7 @@ import turtle as trtl
 import random
 import math
 
-velocity = 3
+velocity = 6
 angleGoing = 0
 
 pongBall = trtl.Turtle()
@@ -12,12 +12,34 @@ pongBall.forward(3)
 pongBall.speed(0)
 pongBall.penup()
 
+increment = 0
+enemyPaddle = []
+for i in range(5):
+    newTurtle = trtl.Turtle()
+    newTurtle.shape("square")
+    newTurtle.penup()
+    newTurtle.speed(0)
+    newTurtle.goto(-220, -40 + increment)
+    increment += 20
+    enemyPaddle.append(newTurtle)
+
+enemyPaddleX = enemyPaddle[2].xcor()
+enemyPaddleY = enemyPaddle[2].ycor()
+
 angleGoing = random.randint(0, 359)
 angleGoing = float(math.radians(angleGoing))
 xVelocity = float(math.cos(angleGoing) * velocity)
 yVelocity = float(math.sin(angleGoing) * velocity)
 
 while True:
+    enemyPaddleX = enemyPaddle[2].xcor()
+    enemyPaddleY = enemyPaddle[2].ycor()
+    if (enemyPaddleY < pongBall.ycor()):
+        for i in range(len(enemyPaddle)):
+            enemyPaddle[i].goto(enemyPaddle[i].xcor(), enemyPaddle[i].ycor() + 2)
+    elif (enemyPaddleY > pongBall.ycor()):
+        for i in range(len(enemyPaddle)):
+            enemyPaddle[i].goto(enemyPaddle[i].xcor(), enemyPaddle[i].ycor() - 2)
     pongBall.goto(pongBall.xcor() + xVelocity, pongBall.ycor() + yVelocity)
     if (math.fabs(pongBall.ycor()) >= 200):
         yVelocity = yVelocity * -1
@@ -30,7 +52,15 @@ while True:
             xVelocity = float(math.cos(angleGoing) * velocity)
             yVelocity = float(math.sin(angleGoing) * velocity)
     elif (math.fabs(pongBall.xcor()) >= 200):
-        xVelocity = xVelocity * -1
+        if (pongBall.xcor() < 0 and math.fabs(enemyPaddleY - pongBall.ycor()) <= 52):
+            xVelocity = xVelocity * -1
+        else:
+            pongBall.goto(0, 0)
+            angleGoing = random.randint(0, 359)
+            angleGoing = float(math.radians(angleGoing))
+            xVelocity = float(math.cos(angleGoing) * velocity)
+            yVelocity = float(math.sin(angleGoing) * velocity)
+
             
 
 
